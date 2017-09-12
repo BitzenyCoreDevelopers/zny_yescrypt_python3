223053b909b8b33ea478e46658a71dfa4fbaa1b4
脇山P <wakiyamap@gmail.com>
2017-09-12 10:42:56 +0900

    python3 に対応

--- yescrypt.c
+++ yescrypt.c
@@ -113,15 +113,27 @@ static PyObject *yescrypt_getpowhash(PyObject *self, PyObject *args)
 {
     char *output;
     PyObject *value;
+#if PY_MAJOR_VERSION >= 3
+    PyBytesObject *input;
+#else
     PyStringObject *input;
+#endif
     if (!PyArg_ParseTuple(args, "S", &input))
         return NULL;
     Py_INCREF(input);
     output = PyMem_Malloc(32);
 
+#if PY_MAJOR_VERSION >= 3
+    yescrypt_hash((char *)PyBytes_AsString((PyObject*) input), output);
+#else
     yescrypt_hash((char *)PyString_AsString((PyObject*) input), output);
+#endif
     Py_DECREF(input);
+#if PY_MAJOR_VERSION >= 3
+    value = Py_BuildValue("y#", output, 32);
+#else
     value = Py_BuildValue("s#", output, 32);
+#endif
     PyMem_Free(output);
     return value;
 }
@@ -131,6 +143,22 @@ static PyMethodDef YescryptMethods[] = {
     { NULL, NULL, 0, NULL }
 };
 
+#if PY_MAJOR_VERSION >= 3
+static struct PyModuleDef YescryptModule = {
+    PyModuleDef_HEAD_INIT,
+    "yescrypt_hash",
+    "...",
+    -1,
+    YescryptMethods
+};
+
+PyMODINIT_FUNC PyInit_zny_yescrypt(void) {
+    return PyModule_Create(&YescryptModule);
+}
+
+#else
+
 PyMODINIT_FUNC initzny_yescrypt(void) {
     (void) Py_InitModule("zny_yescrypt", YescryptMethods);
 }
+#endif
